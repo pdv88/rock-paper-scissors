2e20fc7263156e317ed09efb00c0f78f80cfc1b2
pdv88 <888pdv@gmail.com>
2022-10-21 21:46:24 -0500

    Remake function playRound to work with buttons Finish game interface Make a restart game button

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
+var computerScore = 0
+var playerScore = 0
 const options = ["rock", "paper", "scissors"]
+
+const scoreboard = document.querySelector('#scoreboard')
+const gameMessage = document.querySelector('#gameMessage')
+const weaponMenu = document.querySelector('#weaponMenu')
+const weapon = document.querySelector('.weapon')
+const score = document.createElement('div')
+const gameWinner = document.createElement('div')
+const roundWinner = document.createElement('div')
+const restartGame = document.createElement('button')
 
 function computerChoice() {
     var choice = options[Math.floor(Math.random() * options.length)]
     return choice
 }
 
-function playerChoice(string) {
-    return string
-}
-
-var computerScore = 0
-var playerScore = 0
-
-var rockBtn = document.getElementById("rock")
-    rockBtn.addEventListener("click", playRockRound())
-
-var paperBtn = document.getElementById("paper")
-    paperBtn.addEventListener("click", playPaperRound())
-
-var scissorsBtn = document.getElementById("scissors")
-    scissorsBtn.addEventListener("click", playScissorsRound())
-
-function playRockRound(computerSelection) {
-    if(computerSelection == "rock") { 
-        console.log("It's a draw")
-    } else if(computerSelection == "paper"){
-        console.log("You lost :( paper beats rock" )
-        computerScore++
-    } else {
-        console.log("You won! :) rock beats scissors")
-        playerScore++
+function playRound (playerSelection){
+    const computerSelection = computerChoice()
+    
+    if (playerSelection == computerSelection){
+        roundWinner.textContent = "it's a tie"   
+        gameMessage.appendChild(roundWinner)
+        } else if (playerSelection == "rock" && computerSelection == "scissors" || 
+        playerSelection == "paper" && computerSelection == "rock" ||
+        playerSelection == "scissors" && computerSelection == "paper"){
+            playerScore++
+            roundWinner.textContent = `You win!! ${playerSelection} beats ${computerSelection}`
+            gameMessage.appendChild(roundWinner)
+        } else {
+            computerScore++
+            roundWinner.textContent = `You lose :( ${computerSelection} beats ${playerSelection}`   
+            gameMessage.appendChild(roundWinner)
+        }
+        score.textContent = `Player ${playerScore} Computer ${computerScore}`
+        scoreboard.appendChild(score)
+    if (computerScore == 5 || playerScore == 5){
+        if (computerScore == 5){
+            gameWinner.textContent = 'Game Over !!!! Computer won the game :('
+            gameMessage.appendChild(gameWinner)
+        } else if (playerScore == 5){
+            gameWinner.textContent = 'Game Over !!!! You won the game!! :)'
+            gameMessage.appendChild(gameWinner)
+        }
+        restartGame.textContent = 'Restart Game'
+        restartGame.addEventListener('click', reset)
+        weaponMenu.appendChild(restartGame)
+        var weapons = document.getElementsByClassName("weapon");
+            for(var i = 0; i < weapons.length; i++) {
+            weapons[i].disabled = true;
+            }   
+        // weapon.disabled = true
     }
-    console.log("computer " + computerScore + " Player " + playerScore)
-    console.log("---------------")
 }
 
-function playPaperRound(computerSelection) {
-    if(computerSelection == "paper") { 
-        console.log("It's a draw")
-    } else if(computerSelection == "scissors"){
-        console.log("You lost :( scissors beats paper" )
-        computerScore++
-    } else {
-        console.log("You won! :) paper beats rock")
-        playerScore++
-    }
-    console.log("computer " + computerScore + " Player " + playerScore)
-    console.log("---------------")
+function reset(){
+    playerScore = 0
+    computerScore = 0
+    gameMessage.removeChild(gameWinner)
+    gameMessage.removeChild(roundWinner)
+    scoreboard.removeChild(score)
+    weaponMenu.removeChild(restartGame)
+    var weapons = document.getElementsByClassName("weapon");
+            for(var i = 0; i < weapons.length; i++) {
+            weapons[i].disabled = false;
+            }
+    // weapon.disabled = false
 }
-
-function playScissorsRound(computerSelection) {
-    if(computerSelection == "scissors") { 
-        console.log("It's a draw")
-    } else if(computerSelection == "rock"){
-        console.log("You lost :( rock beats scissors" )
-        computerScore++
-    } else {
-        console.log("You won! :) scissors beats paper")
-        playerScore++
-    }
-    console.log("computer " + computerScore + " Player " + playerScore)
-    console.log("---------------")
-}
-
 
 
 
